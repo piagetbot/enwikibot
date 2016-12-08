83396a4e1163024dbae823b67da7464330d6ee19
DatGuy1 <datguysteam@gmail.com>
2016-12-08 19:53:00 +0100

    basics

--- template-adder.py
+++ template-adder.py
@@ -1,1 +1,20 @@
-def getWarningLevel(user, content=None):
+from wikitools import *
+import re
+
+site = wiki.Wiki()
+site.login('username', 'password') #For later
+
+def getWarningLevel(user=None, content=None):
+  params = {'action':'query',
+            'prop':'revisions',
+            'titles':user,
+            'rvdir':'newer',
+            'rvprop':'content'
+  }
+  req = api.APIRequest(site, params)
+  content = req.query(False)
+
+#In main() later, it would be:
+#if '<!-- Template:uw-\D*2 -->' in content:
+# continue
+#Or similar
