4bb60a269db726710f1129951f93e4d0aff37fd5
tools.piagetenwiki <tools.piagetenwiki@tools-bastion-03.tools.eqiad.wmflabs>
2016-12-09 22:07:41 +0000

    Created code to listen for user talk page recent changes events

---
+++ rcstreamlistener.py
@@ -0,0 +1,20 @@
+# rcstreamlistener.py
+import urllib3.contrib.pyopenssl
+import logging
+from socketIO_client import SocketIO, BaseNamespace
+
+urllib3.contrib.pyopenssl.inject_into_urllib3()
+#logging.basicConfig(level=logging.DEBUG)
+
+class MainNamespace(BaseNamespace):
+    def on_change(self, change): 
+        if change['namespace'] == 3:
+            print 'Page: ' + change['title']
+
+    def on_connect(self):
+        self.emit('subscribe', 'en.wikipedia.org')
+
+socketIO = SocketIO('https://stream.wikimedia.org')
+socketIO.define(MainNamespace, '/rc')
+
+socketIO.wait()
